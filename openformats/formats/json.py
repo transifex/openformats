b33baaa200712f61f6ec70a9006bd90b56ed396b
Konstantinos Bairaktaris <kbairak@transifex.com>
2016-03-07 13:13:22 +0200

    library-backed and dumb implementations of JSON handler

---
+++ openformats/formats/json.py
@@ -0,0 +1,158 @@
+from __future__ import absolute_import
+
+import json
+from collections import OrderedDict
+
+from ..handlers import Handler
+from ..strings import OpenString
+from ..transcribers import Transcriber
+from ..utils.json import DumbJson
+from ..exceptions import ParseError
+
+
+class JsonHandler_(object):
+    name = "json"
+    extension = "json"
+
+    def parse(self, content):
+        parsed = json.loads(content, object_pairs_hook=OrderedDict)
+        assert isinstance(parsed, dict)
+
+        template = OrderedDict()
+
+        self.stringset = []
+
+        self._extract_dict(parsed, nest=None, template=template)
+
+        return json.dumps(template, indent=2), self.stringset
+
+    def _extract_dict(self, parsed, nest, template):
+        for key, value in parsed.iteritems():
+            if nest is None:
+                string_key = key
+            else:
+                string_key = "{}.{}".format(nest, key)
+            if isinstance(value, unicode):
+                string = OpenString(string_key, value)
+                template[key] = string.template_replacement
+                self.stringset.append(string)
+            elif isinstance(value, dict):
+                self._extract_dict(value,
+                                   nest=string_key,
+                                   template=template.setdefault(key,
+                                                                OrderedDict()))
+            elif isinstance(value, list):
+                self._extract_list(value, nest=string_key,
+                                   template=template.setdefault(key, []))
+
+    def _extract_list(self, parsed, nest, template):
+        for i, list_item in enumerate(parsed):
+            string_key = "{}..{}..".format(nest, i)
+            if isinstance(list_item, unicode):
+                string = OpenString(string_key, list_item)
+                self.stringset.append(string)
+                template.append(string.template_replacement)
+            elif isinstance(list_item, dict):
+                template.append(OrderedDict())
+                self._extract_dict(list_item, nest="{}..{}..".format(nest, i),
+                                   template=template[-1])
+            elif isinstance(list_item, list):
+                template.append([])
+                self._extract_list(list_item, nest="{}..{}..".format(nest, i),
+                                   template=template[-1])
+
+    def compile(self, template, stringset):
+        parsed = json.loads(template, object_pairs_hook=OrderedDict)
+        self.stringset = stringset
+        self.stringset_index = 0
+
+        self._intract(parsed)
+
+        return json.dumps(parsed, indent=2)
+
+    def _intract(self, parsed):
+        if isinstance(parsed, dict):
+            iterator = parsed.iteritems()
+        elif isinstance(parsed, list):
+            iterator = enumerate(parsed)
+
+        to_delete = []
+        for key, value in iterator:
+            next_string = self._get_next_string()
+            if isinstance(value, unicode):
+                if (next_string and
+                        next_string.template_replacement == value):
+                    parsed[key] = next_string.string
+                    self.stringset_index += 1
+                else:
+                    to_delete.append(key)
+            else:
+                all_deleted = self._intract(value)
+                if all_deleted:
+                    to_delete.append(key)
+
+        all_deleted = len(to_delete) == len(parsed)
+
+        for key in to_delete[::-1]:
+            del parsed[key]
+
+        return all_deleted
+
+    def _get_next_string(self):
+        try:
+            return self.stringset[self.stringset_index]
+        except IndexError:
+            return None
+
+
+class JsonHandler(Handler):
+    name = "json"
+    extension = "json"
+
+    def parse(self, content):
+        self.transcriber = Transcriber(content)
+        source = self.transcriber.source
+        self.stringset = []
+
+        parsed = DumbJson(source)
+        self.extract(parsed)
+        self.transcriber.copy_until(len(source))
+
+        return self.transcriber.get_destination(), self.stringset
+
+    def extract(self, parsed, nest=None):
+        if parsed.type == dict:
+            for key, key_position, value, value_position in parsed:
+                if nest is not None:
+                    key = "{}.{}".format(nest, key)
+                if isinstance(value, unicode):
+                    openstring = OpenString(key, value)
+                    self.transcriber.copy_until(value_position)
+                    self.transcriber.add(openstring.template_replacement)
+                    self.transcriber.skip(len(value))
+                    self.stringset.append(openstring)
+                elif value.type in (dict, list):
+                    self.extract(value, key)
+                else:
+                    raise ParseError("Invalid JSON")
+        elif parsed.type == list:
+            for index, (item, item_position) in enumerate(parsed):
+                if nest is None:
+                    key = "..{}..".format(index)
+                else:
+                    key = "{}..{}..".format(nest, index)
+                if isinstance(item, unicode):
+                    openstring = OpenString(key, item)
+                    self.transcriber.copy_until(item_position)
+                    self.transcriber.add(openstring.template_replacement)
+                    self.transcriber.skip(len(item))
+                    self.stringset.append(openstring)
+                elif item.type in (dict, list):
+                    self.extract(item, key)
+                else:
+                    raise ParseError("Invalid JSON")
+        else:
+            raise ParseError("Invalid JSON")
+
+    def compile(self, template, stringset):
+        return template
---
+++ openformats/utils/json.py
@@ -0,0 +1,134 @@
+class DumbJson(object):
+    def __init__(self, source, start=0):
+        self.source = source
+        starting_symbol, self.start = self._find_next('{[', start)
+        if starting_symbol == '{':
+            self.type = dict
+        elif starting_symbol == '[':
+            self.type = list
+
+    def wrong__iter__(self):
+        # the '_p' suffix means "position"
+        opening_bracket, opening_bracket_p = self._find_next('{[')
+        if opening_bracket == '{':
+            while True:
+                # lets find our key
+                _, start_key_quote_p = self._find_next('"',
+                                                       opening_bracket_p + 1)
+                key_p = start_key_quote_p + 1
+                _, end_key_quote_p = self._find_next('"', key_p + 1)
+                key = self.source[key_p:end_key_quote_p]
+                _, colon_p = self._find_next(':', end_key_quote_p + 1)
+                start_value, start_value_p = self._find_next('"{[',
+                                                             colon_p + 1)
+                if start_value == '"':
+                    # We found a string!
+                    _, start_value_quote_p = self._find_next('"',
+                                                             start_value_p + 1)
+                    value_p = start_value_p + 1
+                    _, end_value_quote_p = self._find_next('"', value_p)
+                    value = self.source[value_p:end_value_quote_p]
+                    yield key, key_p, value, value_p
+                    next_symbol, next_symbol_p = self._find_next(
+                        ',}', end_value_quote_p + 1,
+                    )
+                    if next_symbol == ',':
+                        opening_bracket_p = next_symbol_p
+                        continue
+                    elif next_symbol == '}':
+                        break
+                elif start_value in ('{', '['):
+                    opening_bracket_p = start_value_p
+                    continue
+        elif opening_bracket == '[':
+            while True:
+                # lets find our item
+                start_item_quote, start_item_quote_p = self._find_next(
+                    '"{[', opening_bracket_p + 1,
+                )
+                if start_item_quote == '"':
+                    item_p = start_item_quote_p + 1
+                    _, end_item_quote_p = self._find_next('"', item_p)
+                    item = self.source[item_p:end_item_quote_p]
+                    yield item, item_p
+                    next_symbol, next_symbol_p = self._find_next(
+                        ',]', end_item_quote_p + 1,
+                    )
+                    if next_symbol == ',':
+                        opening_bracket_p = next_symbol_p + 1
+                        continue
+                    elif next_symbol == ']':
+                        break
+                elif start_item_quote in ('{', '['):
+                    opening_bracket_p = next_symbol_p
+                    continue
+
+    def __iter__(self):
+        if self.type == dict:
+            for what in self._iter_dict():
+                yield what
+        elif self.type == list:
+            for what in self._iter_list():
+                yield what
+
+    def _iter_dict(self):
+        start = self.start + 1
+        while True:
+            # Lets find our key
+            _, start_key_quote_p = self._find_next('"', start)
+            key_p = start_key_quote_p + 1
+            _, end_key_quote_p = self._find_next('"', key_p)
+            key = self.source[key_p:end_key_quote_p]
+            _, colon_p = self._find_next(':', end_key_quote_p + 1)
+            value_start, value_start_p = self._find_next('"{[', colon_p + 1)
+            if value_start == '"':
+                # We found a string!
+                value_p = value_start_p + 1
+                _, value_end_quote_p = self._find_next('"', value_p)
+                value = self.source[value_p:value_end_quote_p]
+                yield key, key_p, value, value_p
+                next_p = value_end_quote_p + 1
+            elif value_start in ('{', '['):
+                # We found an embedded, lets return an instance of ourselves
+                embedded = DumbJson(self.source, value_start_p)
+                yield key, key_p, embedded, value_start_p
+                next_p = embedded.end + 1
+            next_symbol, next_symbol_p = self._find_next(',}', next_p)
+            if next_symbol == ',':
+                start = next_symbol_p + 1
+            elif next_symbol == '}':
+                self.end = next_symbol_p
+                break
+
+    def _iter_list(self):
+        start = self.start + 1
+        while True:
+            # Lets find our items
+            start_item_quote, start_item_quote_p = self._find_next('"{[',
+                                                                   start)
+            if start_item_quote == '"':
+                # We found a string!
+                item_p = start_item_quote_p + 1
+                _, end_item_quote_p = self._find_next('"', item_p)
+                item = self.source[item_p:end_item_quote_p]
+                yield item, item_p
+                next_p = end_item_quote_p + 1
+            elif start_item_quote in ('{', '['):
+                # We found an embedded, lets return an instance of ourselves
+                embedded = DumbJson(self.source, start_item_quote_p)
+                yield embedded, start_item_quote_p
+                next_p = embedded.end + 1
+            next_symbol, next_symbol_p = self._find_next(',]', next_p)
+            if next_symbol == ',':
+                start = next_symbol_p + 1
+            elif next_symbol == ']':
+                self.end = next_symbol_p
+                break
+
+    def _find_next(self, symbols, start=0):
+        symbols = {s for s in symbols}
+        for ptr in xrange(start, len(self.source)):
+            candidate = self.source[ptr]
+            if candidate in symbols:
+                return candidate, ptr
+        return None, None
