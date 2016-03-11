bb22735b43a2f97aaa6ec69dddf7b61c39a1ae8d
Konstantinos Bairaktaris <kbairak@transifex.com>
2016-03-11 15:33:00 +0200

    Compilation (also removes sections)

--- openformats/formats/json.py
+++ openformats/formats/json.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import json
+import re
 from collections import OrderedDict
 
 from ..handlers import Handler
@@ -155,4 +156,121 @@ class JsonHandler(Handler):
             raise ParseError("Invalid JSON")
 
     def compile(self, template, stringset):
-        return template
+        self.transcriber = Transcriber(template)
+        template = self.transcriber.source
+        self.stringset = stringset
+        self.stringset_index = 0
+
+        parsed = DumbJson(template)
+        self.intract(parsed)
+
+        self.transcriber.copy_until(len(template) - 1)
+        compiled = self.transcriber.get_destination()
+
+        return self.clean_compiled(compiled)
+
+    def intract(self, parsed):
+        at_least_one = False
+        if parsed.type == dict:
+            for key, key_position, value, value_position in parsed:
+                self.transcriber.copy_until(key_position - 1)
+                self.transcriber.mark_section_start()
+                if isinstance(value, unicode):
+                    string = self._get_next_string()
+                    if (string is not None and
+                            value == string.template_replacement):
+                        at_least_one = True
+                        self.transcriber.copy_until(value_position)
+                        self.transcriber.add(string.string)
+                        self.transcriber.skip(len(value))
+                        self.stringset_index += 1
+                    else:
+                        self.transcriber.copy_until(value_position +
+                                                    len(value) + 1)
+                        self.transcriber.mark_section_end()
+                        self.transcriber.remove_section()
+                else:
+                    all_removed = self.intract(value)
+                    if all_removed:
+                        self.transcriber.copy_until(value.end + 1)
+                        self.transcriber.mark_section_end()
+                        self.transcriber.remove_section()
+                    else:
+                        at_least_one = True
+
+        elif parsed.type == list:
+            for item, item_position in parsed:
+                self.transcriber.copy_until(item_position - 1)
+                self.transcriber.mark_section_start()
+                if isinstance(item, unicode):
+                    string = self._get_next_string()
+                    if (string is not None and
+                            item == string.template_replacement):
+                        at_least_one = True
+                        self.transcriber.copy_until(item_position)
+                        self.transcriber.add(string.string)
+                        self.transcriber.skip(len(item))
+                        self.stringset_index += 1
+                    else:
+                        self.transcriber.copy_until(item_position +
+                                                    len(item) + 1)
+                        self.transcriber.mark_section_end()
+                        self.transcriber.remove_section()
+                else:
+                    all_removed = self.intract(item)
+                    if all_removed:
+                        self.transcriber.copy_until(item.end + 1)
+                        self.transcriber.mark_section_end()
+                        self.transcriber.remove_section()
+                    else:
+                        at_least_one = True
+        return not at_least_one
+
+    def clean_compiled(self, compiled):
+        "If sections were removed, clean leftover commas, brackets etc"
+        while True:
+            # First key-value of a dict was removed
+            match = re.search(r'{\s*,', compiled)
+            if match:
+                compiled = "{}{{{}".format(compiled[:match.start()],
+                                           compiled[match.end():])
+                continue
+
+            # Last key-value of a dict was removed
+            match = re.search(r',\s*}', compiled)
+            if match:
+                compiled = "{}}}{}".format(compiled[:match.start()],
+                                           compiled[match.end():])
+                continue
+
+            # First item of a list was removed
+            match = re.search(r'\[\s*,', compiled)
+            if match:
+                compiled = "{}[{}".format(compiled[:match.start()],
+                                          compiled[match.end():])
+                continue
+
+            # Last item of a list was removed
+            match = re.search(r',\s*\]', compiled)
+            if match:
+                compiled = "{}]{}".format(compiled[:match.start()],
+                                          compiled[match.end():])
+                continue
+
+            # Intermediate key-value of a dict or list was removed
+            match = re.search(r',\s*,', compiled)
+            if match:
+                compiled = "{},{}".format(compiled[:match.start()],
+                                          compiled[match.end():])
+                continue
+
+            # No substitutions happened, break
+            break
+
+        return compiled
+
+    def _get_next_string(self):
+        try:
+            return self.stringset[self.stringset_index]
+        except IndexError:
+            return None
