fbb35a7cb5481ee48e8010ebea00f14f35009bcd
Sofia Margariti <sofia@transifex.com>
2018-01-09 13:09:01 +0200

    Try to parse Stories in order they appear in the document

--- openformats/formats/indesign.py
+++ openformats/formats/indesign.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 import io
 
-from base64 import b64encode, b64decode
+from collections import OrderedDict
+from itertools import chain
 from bs4 import BeautifulSoup
 from ucf import UCF
 
@@ -37,12 +38,15 @@ class InDesignHandler(Handler):
         order = 0
 
         idml = UCF(io.BytesIO(content))
+        ordered_stories = self._get_ordered_stories(idml)
 
         # Iterate over the contents of the IDML file
-        for key in idml.keys():
-            if not key.startswith("Stories/"):
+        for key in ordered_stories:
+            try:
+                story_content = idml[key]
+            except KeyError:
                 continue
-            soup = BeautifulSoup(idml[key], "xml")
+            soup = BeautifulSoup(story_content, "xml")
             replacements = []
             # First, we save the strings to the string-set. We don't alter the
             # XML DOM contents because is will cause the for loops to process
@@ -70,6 +74,44 @@ class InDesignHandler(Handler):
         template = out.getvalue()
 
         return template, stringset
+
+    def _get_ordered_stories(self, idml):
+        """
+        Try to find the order the stories appear in the indesign document
+        * Parse designmap.xml to get the spreads in the orderthe appear in the
+          document.
+        * Parse each Spread in order to get all the TextFrame elemements in the
+          order they appear
+        * Get the ParentStory attribute of each TextFrame
+        * Return a list with unique Story keys while maintaining the order in
+          which they were found.
+        """
+        ordered_story_ids = []
+        STORY_KEY = 'Stories/Story_{}.xml'
+        designmap = idml.get('designmap.xml')
+        designmap_soup = BeautifulSoup(designmap, 'xml')
+        masterspreads = designmap_soup.find_all('idPkg:MasterSpread')
+        spreads = designmap_soup.find_all('idPkg:Spread')
+
+        for spread in chain(masterspreads, spreads):
+            spread_key = spread.attrs.get('src')
+            spread_xml = idml.get(spread_key)
+            if not spread_xml:
+                continue
+            spread_content = BeautifulSoup(spread_xml, 'xml')
+            text_frames = spread_content.find_all('TextFrame')
+            ordered_story_ids.extend([
+                frame.attrs.get('ParentStory') for frame in text_frames
+            ])
+
+        unique_story_ids = OrderedDict.fromkeys(ordered_story_ids)
+        unique_stories = [STORY_KEY.format(s) for s in unique_story_ids]
+
+        # In case there are stories that is not referenced by any TextFrame,
+        # append them at the end of the list
+        all_stories = {k for k in idml.keys() if k.startswith('Stories')}
+        unique_stories.extend(all_stories - set(unique_stories))
+        return unique_stories
 
     def _get_all_contents(self, idml):
         """Return an array with all the strings of an IDML file."""
