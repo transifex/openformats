5742fd73fa617d6f359cde37112c8fbe8b7d971f
Konstantinos Bairaktaris <kbairak@transifex.com>
2019-09-19 13:58:21 +0300

    Revert DumbXml to its previous code
    We had included "processing instructions" [1] in the capabilities of
    DumbXml because it kind of is part of the XML specification, it appears
    frequently in .idml files and if a file includes them, the previous
    DumbXml couldn't handle it.
    
    Since the InDesign handler doesn't use DumbXml after all, we revert this
    change.
    
    [1] https://en.wikipedia.org/wiki/Processing_Instruction

--- openformats/utils/xml.py
+++ openformats/utils/xml.py
@@ -354,7 +354,6 @@ class NewDumbXml(object):
         "Special value for None because for some properties, None is valid"
 
     COMMENT = '!--'
-    PROCESSING_INSTRUCTION = '?'
 
     def __init__(self, source, start=0):
         self.source = source
@@ -398,12 +397,6 @@ class NewDumbXml(object):
         if self.source[start:end] == "<!--":
             self._tag = self.COMMENT
             self._process_comment()
-            return self._tag
-
-        end = start + len('<?')
-        if self.source[start:end] == "<?":
-            self._tag = self.PROCESSING_INSTRUCTION
-            self._process_processing_instruction()
             return self._tag
 
         for ptr in six.moves.xrange(self.position + 1, len(self.source)):
@@ -559,8 +552,7 @@ class NewDumbXml(object):
         return self._text
 
     def __iter__(self):
-        if (self.text is None or self.tag == self.COMMENT
-                or self.tag == self.PROCESSING_INSTRUCTION):
+        if self.text is None or self.tag == self.COMMENT:
             return
 
         start = self.text_position + len(self.text)
@@ -623,7 +615,7 @@ class NewDumbXml(object):
                ^^^^^^^^^^^^^^^^^^^^^^^^^
         """
 
-        if self.tag == self.COMMENT or self.tag == self.PROCESSING_INSTRUCTION:
+        if self.tag == self.COMMENT:
             return self.text
         if self.content_end is None:
             return None
@@ -722,22 +714,6 @@ class NewDumbXml(object):
                 return
         raise DumbXmlSyntaxError(u"Comment not closed on line {}".
                                  format(self._find_line_number()))
-
-    def _process_processing_instruction(self):
-        self._attrib_string, self._attributes, self._attrib = "", [], {}
-
-        self._text_position = self.position + len("<?")
-        for ptr in six.moves.xrange(self._text_position, len(self.source)):
-            candidate = self.source[ptr]
-            if candidate == "?" and self.source[ptr:ptr + len("?>")] == "?>":
-                self._content_end = ptr
-                self._text = self.source[self.text_position:ptr]
-                self._tail_position = ptr + len("?>")
-                return
-        raise DumbXmlSyntaxError(
-            u"Processing instruction not closed on line {}".
-            format(self._find_line_number())
-        )
 
     def _find_line_number(self, ptr=None):
         ptr = ptr or self.position
