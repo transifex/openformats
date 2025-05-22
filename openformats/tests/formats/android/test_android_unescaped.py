f1618f225b32ef5d048a1bd99163a63e11217c9d
Dimitris Rongotis <rongotis@transifex.com>
2025-05-22 14:47:01 +0300

    update test

--- openformats/tests/formats/android/test_android_unescaped.py
+++ openformats/tests/formats/android/test_android_unescaped.py
@@ -16,9 +16,9 @@ class AndroidUnescapedTestCase(CommonFormatTestMixin, unittest.TestCase):
     HANDLER_CLASS = AndroidUnescapedHandler
     TESTFILE_BASE = "openformats/tests/formats/android/files"
 
-    def setUp(self):
-        super(AndroidUnescapedTestCase, self).setUp()
-        self.handler = AndroidUnescapedHandler()
+    # def setUp(self):
+    #     super(AndroidUnescapedTestCase, self).setUp()
+    #     self.handler = AndroidUnescapedHandler()
 
     def test_string(self):
         self.maxDiff = None
@@ -47,43 +47,40 @@ class AndroidUnescapedTestCase(CommonFormatTestMixin, unittest.TestCase):
 
     def test_cdata_string(self):
         self.maxDiff = None
-        random_key = generate_random_string()
-        uploaded_string = """<![CDATA[<b>test</b>]]>"""
-        saved_string = """<b>test</b>"""
-        uploaded_openstring = OpenString(random_key, saved_string, order=0)
+        uploaded_openstring = OpenString("onshape_edu_plan", "<b>Onshape Education Plan</b>", order=0)
+        uploaded_openstring2 = OpenString("explore_the_basics", "Explore the basics", order=1)
+        uploaded_openstring3 = OpenString("test", "<b>Onshape Education Plan</b>", order=2)
         uploaded_hash = uploaded_openstring.template_replacement
+        uploaded_hash2 = uploaded_openstring2.template_replacement
+        uploaded_hash3 = uploaded_openstring3.template_replacement
 
-        source_python_template = """
+        source= """
             <resources>
-                <string name="{key}">{string}</string>
+                <string name="onshape_edu_plan"><![CDATA[<b>Onshape Education Plan</b>]]></string>
+                <string name="explore_the_basics">Explore the basics</string>
+                <string name="test"><b>Onshape Education Plan</b></string>
             </resources>
         """
-        cdata_source_python_template = """
+        cdata_source_python_template = f"""
             <resources>
-                <string name="{key}"><![CDATA[{string}]]></string>
+                <string name="{uploaded_openstring.key}"><![CDATA[{uploaded_hash}]]></string>
+                <string name="{uploaded_openstring2.key}">{uploaded_hash2}</string>
+                <string name="{uploaded_openstring3.key}">{uploaded_hash3}</string>
             </resources>
         """
 
-        source = source_python_template.format(key=random_key, string=uploaded_string)
         template, stringset = self.handler.parse(source)
-       
-        compiled = self.handler.compile(template, [uploaded_openstring])
+        compiled = self.handler.compile(template, [uploaded_openstring,uploaded_openstring2,uploaded_openstring3])
         self.assertEqual(
             template,
-            cdata_source_python_template.format(key=random_key, string=uploaded_hash),
+            cdata_source_python_template,
         )
-        self.assertEqual(len(stringset), 1)
+        self.assertEqual(len(stringset), 3)
         self.assertEqual(stringset[0].__dict__, uploaded_openstring.__dict__)
         self.assertEqual(compiled, source)
+        self.assertEqual(self.handler.debug_counter, 3)
 
-    def test_escape(self):
-        rich = '&>"\n\t@? <xliff:g id="1">%1$s &</xliff:g> @ ?'
-        raw = '&amp;&gt;\\"\\n\\t\\@\\? <xliff:g id="1">%1$s &</xliff:g> \\@ \\?'
 
-        self.assertEqual(
-            AndroidUnescapedHandler.escape(rich),
-            raw,
-        )
     def test_escape(self):
         rich = '&>"\n\t@? <xliff:g id="1">%1$s &</xliff:g> @ ?'
         raw = '&amp;&gt;\\"\\n\\t\\@\\? <xliff:g id="1">%1$s &</xliff:g> \\@ \\?'
